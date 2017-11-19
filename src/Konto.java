ebb949aa2733cbacf0fa775619e2f500652281af
Kamil Stasiak <kamil.stasiak97@icloud.com>
2017-11-19 20:19:05 +0100

    final

--- src/Konto.java
+++ src/Konto.java
@@ -1,5 +1,7 @@
 class Konto{
     private int stan;
+    int[] historia = new int[5];
+    static int licznik = 0;
     Konto() { stan = 0; }
     public void operacja(int ile) throws DebetException {
         if (stan + ile >= 0 )
@@ -7,7 +9,8 @@ class Konto{
         else
             throw new DebetException("\n!!!!!!!!!!!!!!!!!!!!!!!!\n" +
                                      "Przekroczono limit konta\n" +
-                                     "!!!!!!!!!!!!!!!!!!!!!!!!\n");
+                                     "!!!!!!!!!!!!!!!!!!!!!!!!");
     }
     public int dajStan() { return stan ; }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,23 @@
 import java.awt.* ;
 import java.awt.event.* ;
+import java.util.ArrayList;
 import javax.swing.*;
 
 public class Main extends JFrame {
 
     Konto konto = new Konto();
+    public ArrayList<Integer> historiaOperacji = new ArrayList<>();
+    int i = historiaOperacji.size();
 
     JTextField
             rezultat = new JTextField(),
             stan = new JTextField(),
-            operacja = new JTextField();
+            operacja = new JTextField(),
+            historia = new JTextField();
     JButton
             polecenie = new JButton("wplata/wyplata"),
-            odblokuj = new JButton("odblokuj");
+            odblokuj = new JButton("odblokuj"),
+            historiaButton = new JButton("Operacja wstecz ");
     JLabel
             stanLable = new JLabel("          Stan : ");
 
@@ -20,20 +25,28 @@ public class Main extends JFrame {
         setTitle("Konto");
         Container c = getContentPane();
         setSize(400 , 600);
-        setLayout(new GridLayout(3 , 2));
+        setLayout(new GridLayout(4 , 2));
         c.add(stanLable);
         c.add(stan);
+
 
         c.add(polecenie);
         c.add(operacja);
 
-        c.add(rezultat);
+
         c.add(odblokuj);
+        c.add(rezultat);
+
+        c.add(historiaButton);
+        c.add(historia);
+
+
 
         stan.setText(String.valueOf(konto.dajStan()));
         operacja.setText("0");
         polecenie.addActionListener(new Operacja());
         odblokuj.addActionListener(new Odblokuj());
+        historiaButton.addActionListener(new Historia());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
@@ -46,6 +59,8 @@ public class Main extends JFrame {
 
     void obliczenia(){
         int ile = Integer.parseInt(operacja.getText());
+        historiaOperacji.add(ile);
+        i=historiaOperacji.size();
         try {
             konto.operacja(ile);
             rezultat.setText("OK");
@@ -63,7 +78,21 @@ public class Main extends JFrame {
         }
     }
 
+    class Historia implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+                if(i>0) {
+                    historia.setText(String.valueOf(historiaOperacji.get(i - 1)));
+                    i--;
+                }
+                else
+                    historia.setText("Brak operacji");
+        }
+    }
+
     class Odblokuj implements  ActionListener{
+
         @Override
         public void actionPerformed(ActionEvent e) {
             operacja.setEnabled(true);
